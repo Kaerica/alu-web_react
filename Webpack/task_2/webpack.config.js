dbf167cb8808cb1bb4b3b81ff0190adcea867d0f
root <e.ishimwe2@alustudent.com>
2026-09-09 20:57:24 +0000

    Add Webpack tasks 0, 1 and task 2 setup

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+
+  entry: './js/dashboard_main.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  optimization: {
+    minimizer: [
+      '...',
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ['gifsicle', { interlaced: true }],
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 5 }],
+              [
+                'svgo',
+                {
+                  plugins: [
+                    {
+                      name: 'preset-default',
+                      params: {
+                        overrides: {
+                          removeViewBox: false,
+                        },
+                      },
+                    },
+                  ],
+                },
+              ],
+            ],
+          },
+        },
+      }),
+    ],
+  },
+};
